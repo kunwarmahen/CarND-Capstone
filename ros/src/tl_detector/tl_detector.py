ebe5abceafe3831e08eed39a9e54a164da33fb80
Mahendra P Singh <kunwarmahen@gmail.com>
2017-09-24 22:05:15 -0400

    Add files via upload
    Updated logic for traffic light and fixed bugs

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -51,6 +51,7 @@ class TLDetector(object):
         self.state_count = 0
 
         rospy.spin()
+		
 
     def pose_cb(self, msg):
         self.pose = msg
@@ -69,6 +70,7 @@ class TLDetector(object):
             msg (Image): image from car-mounted camera
 
         """
+		
         self.has_image = True
         self.camera_image = msg
         light_wp, state = self.process_traffic_lights()
@@ -104,17 +106,19 @@ class TLDetector(object):
         #TODO implement
         dist = float('inf')
         index = 0
+        selected_index = None
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
         for waypoint in waypoints:
             temp_dist = dl(waypoint.pose.pose.position, pose.position)
             if temp_dist < dist:
                dist = temp_dist
-               index = index + 1
+               selected_index = index
+            index = index + 1
 
-            if waypoints[index].pose.pose.position.x < pose.position.x:
-               index = index + 1
+            if waypoints[selected_index].pose.pose.position.x < pose.position.x:
+               selected_index = selected_index + 1
   
-        return index
+        return selected_index
 
 
     def project_to_image_plane(self, point_in_world):
@@ -187,56 +191,57 @@ class TLDetector(object):
         """
         light = None
         car_position = None
+        light_index = None
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
-        #if(self.pose and self.waypoints):
-        #    car_position = self.get_closest_waypoint(self.pose.pose, self.waypoints)
+        
+        if(self.pose is not None and self.lights is not None and self.waypoints is not None):
+            light_index = self.closest_traffic_light(self.pose.pose, self.lights)
 
         #TODO find the closest visible traffic light (if one exists)
-        if car_position:
-            light_index = self.closest_traffic_light(car_position)
+        if light_index is not None:
             light = self.lights[light_index]
             stop_line_position = stop_line_positions[light_index]
-            light_wp = self.closest_stop_line_position(stop_line_position)			
+            light_wp = self.closest_stop_line_position(stop_line_position)
    
         if light:
             #state = self.get_light_state(light)
             state = light.state
             return light_wp, state
-        #self.waypoints = None
         return -1, TrafficLight.UNKNOWN
 
     def closest_stop_line_position(self, stop_line_position):
         dist = float('inf')
         index = 0
+        selected_index = None
         dl = lambda a, b: math.sqrt((a.x-b[0])**2 + (a.y-b[1])**2)
         for waypoint in self.waypoints:
             temp_dist = dl(waypoint.pose.pose.position, stop_line_position)
             if temp_dist < dist:
                dist = temp_dist
-               index = index + 1
+               selected_index = index
+            index = index + 1
 
-            if waypoints[index].pose.pose.position.x < stop_line_position[0]:
-               index = index + 1
+            if self.waypoints[selected_index].pose.pose.position.x < stop_line_position[0]:
+               selected_index = selected_index + 1
   
-        return index		
+        return selected_index		
 
-    def closest_traffic_light(self, car_position):
+    def closest_traffic_light(self, pose, lights):
         dist = float('inf')
         index = 0
-        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-        for light in self.lights:
-            temp_dist = dl(self.waypoints[car_position].pose.pose.position, light.pose.pose.position)
-            if temp_dist < dist:
+        selected_index = None
+        dl = lambda a, b: a.x-b.x
+        for light in lights:
+            temp_dist = dl(light.pose.pose.position, pose.position)
+            if temp_dist> 0 and temp_dist < dist:
                dist = temp_dist
-               index = index + 1
-
-            #if self.waypoints[car_position].pose.pose.position.x > lights[index].pose.pose.position.x:
-            #   index = index + 1
-  
-        return index	
-
+               selected_index = index
+            index = index + 1   
+        return selected_index	
+		
+		
 		
 if __name__ == '__main__':
     try:
